df571df8e3d4847b3de9e00a359c29e33feadd05
RomanChudov <rc3670h@gmail.com>
2024-11-19 14:22:27 +0000

    New new hope

--- build/lib/components/keywords.js
+++ build/lib/components/keywords.js
@@ -21,18 +21,4 @@ export function renderKeywords(root, keywords) {
   }
   
 
-  /*
-  <div class="flip-card">
-    <div class="flip-card-inner">
-        <div class="flip-card-front">
-        <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;">
-        </div>
-        <div class="flip-card-back">
-        <h1>John Doe</h1> 
-        <p>Architect & Engineer</p> 
-        <p>We love that guy</p>
-        </div>
-    </div>
-  </div>
-  */
-}
+ }
